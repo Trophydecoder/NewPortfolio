c7059dd670a02932d5236808dccc286b4e7d1f83
Trophydecoder <trophydecoder@gmail.com>
2025-02-10 02:47:31 +0200

    About ME added

--- src/app/pages/aboutpage/aboutpage.component.ts
+++ src/app/pages/aboutpage/aboutpage.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from "../../components/nav-bar/nav-bar.component";
 import { AboutSectionComponent } from '../../aboutpagecomponents/about-section/about-section.component';
-
+import { AboutServicesComponent } from '../../aboutpagecomponents/about-services/about-services.component';
 @Component({
   selector: 'app-aboutpage',
   standalone: true,
-  imports:[NavBarComponent , AboutSectionComponent],
+  imports:[NavBarComponent , AboutSectionComponent , AboutServicesComponent],
   templateUrl: './aboutpage.component.html',
   styleUrl: './aboutpage.component.scss'
 })
