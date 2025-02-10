21f68e77ef851731c7ae1d77eefa3c09fbc9dc43
Trophydecoder <trophydecoder@gmail.com>
2025-02-10 15:12:17 +0200

    new added Bg on projects

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,11 +6,12 @@ import { ProjectsComponent } from "../../components/projects/projects.component"
 import { ContactComponent } from "../../components/contact/contact.component";
 import { IconsComponent } from "../../components/icons/icons.component";
 import { FooterComponent } from "../../components/footer/footer.component";
+import { AboutServicesComponent } from '../../aboutpagecomponents/about-services/about-services.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavBarComponent, CarouselComponent, AboutmeComponent, ProjectsComponent, ContactComponent, IconsComponent, FooterComponent],
+  imports: [NavBarComponent, CarouselComponent, AboutmeComponent, ProjectsComponent, ContactComponent, IconsComponent, FooterComponent , AboutServicesComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
